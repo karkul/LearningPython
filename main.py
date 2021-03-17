a28d3b12bc2839425b010a7e700da5142ed2a0b5
Ramon Morales <ramon.morales-lopez@t-systems.com>
2021-03-17 12:14:05 -0600

    list:   - working with lists and for loop   - iteration of values

--- main.py
+++ main.py
@@ -1,31 +1,8 @@
-command = ""
-started = False
+numbers = [7, 2, 5, 10, 33]
+max_value = 0
 
-while True:
-    command = input(">").lower()
+for number in numbers:
+    if number > max_value:
+        max_value = number
 
-    if command == "start":
-        if started:
-            print("The car is already started!")
-        else:
-            started = True
-            print("Car started... Ready to go!")
-    elif command == "stop":
-        if not started:
-            print("The car is already stopped!")
-        else:
-            started = False
-            print("Car stopped.")
-    elif command == "help":
-        print("""\
-start - to start the car
-stop - to stop the car
-quit - to exit
-        """)
-    elif command == "quit":
-        print("Bye!")
-        break
-    else:
-        print("I don't understand that...")
-
-
+print(max_value)
