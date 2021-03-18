f6bcb904f3db1e7cab2ae7f0378fa8ec7b932103
Ramon Morales <ramon.morales-lopez@t-systems.com>
2021-03-18 12:46:55 -0600

    Dictionaries:   - Working with dictionaries and their functions

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-coordinates = [1, 2, 3]
-x, y, z = coordinates
-print(f"X:{x} Y:{y} Z:{z}")
+message = input("> ")
+words = message.split()
+
+emojis = {
+    ":)": "🙂",
+    ":(": "☹"
+}
+
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+
+print(output)
