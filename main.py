cf44649feb911ebf758bcfcd0c3b72c11b8a80c4
Ramon Morales <ramon.morales-lopez@t-systems.com>
2021-03-17 22:25:52 -0600

    list & Tuples:   - Using unpacking   - using list functions

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-numbers = [2, 2, 4, 6, 3, 4, 6, 1]
-uniques = []
-
-for number in numbers:
-    if number not in uniques:
-        uniques.append(number)
-
-uniques.sort()
-print(uniques)
+coordinates = [1, 2, 3]
+x, y, z = coordinates
+print(f"X:{x} Y:{y} Z:{z}")
